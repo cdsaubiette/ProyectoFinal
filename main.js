d396f2e52536ad1112c664460a1b86b48a498c8e
David Saubiette <cdsaubiette@hotmail.com>
2023-11-25 20:17:07 -0300

    confeti de java

---
+++ main.js
@@ -0,0 +1,9 @@
+        document.addEventListener('DOMContentLoaded', () => {
+            const jsConfetti = new JSConfetti()
+        
+            jsConfetti.addConfetti({
+                emojis: ['🍷', '⚡️', '🍷', '🍻', '💥', '😎', '✨','🎉','⚡'],
+            })
+        })
+        
+    
